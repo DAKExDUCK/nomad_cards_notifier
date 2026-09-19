c7c33a2050e50c512643046fdf85322518508d05
DAKExDUCK <arsengabdulin228@gmail.com>
2026-09-19 18:33:43 +0500

    Add database configuration and dependencies

--- config.py
+++ config.py
@@ -10,7 +10,14 @@ TOKEN = str(get_from_env("TOKEN"))
 
 RATE = 0.25
 
-TEST = bool(get_from_env(field="TEST", default="0", value_type=int))
-
 TZ_RAW = str(get_from_env("TZ", "Asia/Aqtobe"))
 TZ = timezone(TZ_RAW)
+
+# Database config
+DB_USER = str(get_from_env("DB_USER", "postgres"))
+DB_PASSWORD = str(get_from_env("DB_PASSWORD", "postgres"))
+DB_HOST = str(get_from_env("DB_HOST", "localhost"))
+DB_PORT = str(get_from_env("DB_PORT", "5432"))
+DB_NAME = str(get_from_env("DB_NAME", "nomad_cards"))
+
+DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
